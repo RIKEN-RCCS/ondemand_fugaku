ab42d18b5dcf8e7b9d788f7296c188b101677cf8
Masahiro Nakao <masahiro.nakao@riken.jp>
2023-04-12 22:11:19 +0900

    Code cleanyp

--- misc/form.rb
+++ misc/form.rb
@@ -1,5 +1,6 @@
 require 'fileutils'
 
+$attr         = ""
 BASE_DIR      = ENV['HOME'] + "/ondemand/cache/"
 GROUPS_CACHE  = BASE_DIR + "groups.cache"
 EXCLUDED_DIRS = ["f-op", "fugaku", "isv"]
@@ -190,18 +191,18 @@ def get_groups_fdirs()
   return "'[" + dirs.chop + "]'"
 end
 
-def form_cluster(attr, name = "")
-  attr <<<<"EOF"
+def form_cluster(name = "")
+  $attr <<<<"EOF"
   cluster:
     widget: hidden_field
 EOF
-  attr << "    value: #{name}\n" if name != ""
+  $attr << "    value: #{name}\n" if name != ""
 
   return "- cluster"
 end
 
-def form_queue(attr, name = "")
-  attr <<<<"EOF"
+def form_queue(name = "")
+  $attr <<<<"EOF"
   queue:
     label: Queue
     widget: select
@@ -209,44 +210,44 @@ def form_queue(attr, name = "")
 EOF
 
   if name == ""
-    attr << FUGAKU_SMALL
-    attr << PREPOST_GPU1
-    attr << PREPOST_GPU2
-    attr << PREPOST_MEM1
-    attr << PREPOST_MEM2
-    attr << PREPOST_RESERVED
+    $attr << FUGAKU_SMALL
+    $attr << PREPOST_GPU1
+    $attr << PREPOST_GPU2
+    $attr << PREPOST_MEM1
+    $attr << PREPOST_MEM2
+    $attr << PREPOST_RESERVED
   elsif name == "fugaku_small"
-    attr << FUGAKU_SMALL
+    $attr << FUGAKU_SMALL
   elsif name == "prepost"
-    attr << PREPOST_GPU1
-    attr << PREPOST_GPU2
-    attr << PREPOST_MEM1
-    attr << PREPOST_MEM2
-    attr << PREPOST_RESERVED
+    $attr << PREPOST_GPU1
+    $attr << PREPOST_GPU2
+    $attr << PREPOST_MEM1
+    $attr << PREPOST_MEM2
+    $attr << PREPOST_RESERVED
   elsif name == "gpu"
-    attr << PREPOST_GPU1
-    attr << PREPOST_GPU2
+    $attr << PREPOST_GPU1
+    $attr << PREPOST_GPU2
   elsif name == "workflow"
-    attr << PREPOST_RESERVED
-    attr << PREPOST_GPU1
-    attr << PREPOST_GPU2
-    attr << PREPOST_MEM1
-    attr << PREPOST_MEM2
+    $attr << PREPOST_RESERVED
+    $attr << PREPOST_GPU1
+    $attr << PREPOST_GPU2
+    $attr << PREPOST_MEM1
+    $attr << PREPOST_MEM2
   end
   
   return "- queue"
 end
 
-def form_volume(attr)
-  attr <<<<"EOF"
+def form_volume()
+  $attr <<<<"EOF"
   volume:
     widget: hidden_field
 EOF
   return "- volume"
 end
 
-def form_group(attr)
-  attr <<<<"EOF"
+def form_group()
+  $attr <<<<"EOF"
   group:
     label: Group
     widget: select
@@ -257,14 +258,14 @@ EOF
     EXCLUDED_DIRS.each do |d|
       flag = false if n[0] == d
     end
-    attr << "      - [\"" + n[0] + "\" , \"" + n[0] + "\", " + "data-set-volume: \"" + n[3] + "\"]\n" if flag
+    $attr << "      - [\"" + n[0] + "\" , \"" + n[0] + "\", " + "data-set-volume: \"" + n[3] + "\"]\n" if flag
   end
   
   return "- group"
 end
 
-def form_fugaku_hours(attr)
-  attr <<<<"EOF"
+def form_fugaku_hours()
+  $attr <<<<"EOF"
   fugaku_hours:
     label: Elapsed time (1 - 72 hours)
     widget: number_field
@@ -277,8 +278,8 @@ EOF
   return "- fugaku_hours"
 end
 
-def form_fugaku_nodes(attr)
-  attr <<<<"EOF"
+def form_fugaku_nodes()
+  $attr <<<<"EOF"
   fugaku_nodes:
     label: Number of nodes (1 - 384)
     widget: number_field
@@ -291,12 +292,12 @@ EOF
   return "- fugaku_nodes"
 end
 
-def form_fugaku_procs(attr)
-  attr <<<<"EOF"
+def form_fugaku_procs()
+  $attr <<<<"EOF"
   fugaku_procs:
     label: Total number of processes (1 - 18,432)
     widget: number_field
-    value: 48
+    value: 1
     min: 1
     max: 18432
     step: 1
@@ -307,8 +308,8 @@ EOF
   return "- fugaku_procs"
 end
 
-def form_fugaku_threads(attr)
-  attr <<<<"EOF"
+def form_fugaku_threads()
+  $attr <<<<"EOF"
   fugaku_threads:
     label: Number of threads (1 - 48)
     widget: number_field
@@ -323,8 +324,8 @@ EOF
   return "- fugaku_threads"
 end
 
-def form_prepost1_hours(attr)
-  attr <<<<"EOF"
+def form_prepost1_hours()
+  $attr <<<<"EOF"
   prepost1_hours:
     label: Elapsed time (1 - 3 hours)
     widget: number_field
@@ -337,8 +338,8 @@ EOF
   return "- prepost1_hours"
 end
 
-def form_prepost2_hours(attr)
-  attr <<<<"EOF"
+def form_prepost2_hours()
+  $attr <<<<"EOF"
   prepost2_hours:
     label: Elapsed time (1 - 24 hours)
     widget: number_field
@@ -351,8 +352,8 @@ EOF
   return "- prepost2_hours"
 end
 
-def form_reserved_hours(attr)
-  attr <<<<"EOF"
+def form_reserved_hours()
+  $attr <<<<"EOF"
   reserved_hours:
     label: Elapsed time (1 - 720 hours)
     widget: number_field
@@ -365,8 +366,8 @@ EOF
   return "- reserved_hours"
 end
 
-def form_gpu1_cores(attr, min = 1, max = 72)
-  attr <<<<"EOF"
+def form_gpu1_cores(min = 1, max = 72)
+  $attr <<<<"EOF"
   gpu1_cores:
     label: Number of CPU cores (#{min} - #{max})
     widget: number_field
@@ -379,8 +380,8 @@ EOF
   return "- gpu1_cores"
 end
 
-def form_gpu2_cores(attr, min = 1, max = 36)
-  attr <<<<"EOF"
+def form_gpu2_cores(min = 1, max = 36)
+  $attr <<<<"EOF"
   gpu2_cores:
     label: Number of CPU cores (#{min} - #{max})
     widget: number_field
@@ -393,8 +394,8 @@ EOF
   return "- gpu2_cores"
 end
 
-def form_mem1_cores(attr, min = 1, max = 224)
-  attr <<<<"EOF"
+def form_mem1_cores(min = 1, max = 224)
+  $attr <<<<"EOF"
   mem1_cores:
     label: Number of CPU cores (#{min} - #{max})
     widget: number_field
@@ -407,8 +408,8 @@ EOF
   return "- mem1_cores"
 end
 
-def form_mem2_cores(attr, min = 1, max = 56)
-  attr <<<<"EOF"
+def form_mem2_cores(min = 1, max = 56)
+  $attr <<<<"EOF"
   mem2_cores:
     label: Number of CPU cores (#{min} - #{max})
     widget: number_field
@@ -421,8 +422,8 @@ EOF
   return "- mem2_cores"
 end
 
-def form_reserved_cores(attr, min = 1, max = 8)
-  attr <<<<"EOF"
+def form_reserved_cores( min = 1, max = 8)
+  $attr <<<<"EOF"
   reserved_cores:
     label: Number of CPU cores (#{min} - #{max})
     widget: number_field
@@ -435,8 +436,8 @@ EOF
   return "- reserved_cores"
 end
 
-def form_gpu1_memory(attr, min = 5, max = 186)
-  attr <<<<"EOF"
+def form_gpu1_memory(min = 5, max = 186)
+  $attr <<<<"EOF"
   gpu1_memory:
     label: Required memory (#{min} - #{max} GB)
     widget: number_field
@@ -449,8 +450,8 @@ EOF
   return "- gpu1_memory"
 end
 
-def form_gpu2_memory(attr, min = 5, max = 93)
-  attr <<<<"EOF"
+def form_gpu2_memory(min = 5, max = 93)
+  $attr <<<<"EOF"
   gpu2_memory:
     label: Required memory (#{min} - #{max} GB)
     widget: number_field
@@ -463,8 +464,8 @@ EOF
   return "- gpu2_memory"
 end
 
-def form_mem1_memory(attr, min = 5, max = 5020)
-  attr <<<<"EOF"
+def form_mem1_memory(min = 5, max = 5020)
+  $attr <<<<"EOF"
   mem1_memory:
     label: Required memory (#{min} - #{max} GB)
     widget: number_field
@@ -477,8 +478,8 @@ EOF
   return "- mem1_memory"
 end
 
-def form_mem2_memory(attr, min = 5, max = 1500)
-  attr <<<<"EOF"
+def form_mem2_memory(min = 5, max = 1500)
+  $attr <<<<"EOF"
   mem2_memory:
     label: Required memory (#{min} - #{max} GB)
     widget: number_field
@@ -491,8 +492,8 @@ EOF
   return "- mem2_memory"
 end
 
-def form_reserved_memory(attr, min = 5, max = 32)
-  attr <<<<"EOF"
+def form_reserved_memory(min = 5, max = 32)
+  $attr <<<<"EOF"
   reserved_memory:
     label: Required memory (#{min} - #{max} GB)
     widget: number_field
@@ -505,8 +506,8 @@ EOF
   return "- reserved_memory"
 end
 
-def form_gpus_per_node(attr, min = 0, max = 2)
-  attr <<<<"EOF"
+def form_gpus_per_node(min = 0, max = 2)
+  $attr <<<<"EOF"
   gpus_per_node:
     label: Number of GPUs (#{min} - #{max})
     widget: number_field
@@ -519,8 +520,8 @@ EOF
   return "- gpus_per_node"
 end
 
-def form_mode(attr)
-  attr <<<<"EOF"
+def form_mode()
+  $attr <<<<"EOF"
   mode:
     label: Mode
     widget: select
@@ -533,26 +534,26 @@ EOF
   return "- mode"
 end
 
-def form_version(attr, versions)
-  attr <<<<"EOF"
+def form_version(versions)
+  $attr <<<<"EOF"
   version:
     label: Version
     widget: select
     options:
 EOF
   versions.each do |v|
-    attr << "    - [" + v + ", " + v
+    $attr << "    - [" + v + ", " + v
     versions.each do |i|
-      attr << ", data-hide-binary-" + i.delete(".") + ": true " unless i == v
+      $attr << ", data-hide-binary-" + i.delete(".") + ": true " unless i == v
     end
-    attr << "]\n"
+    $attr << "]\n"
   end
   
   return "- version"
 end
 
-def form_binary(attr, version, binaries, value)
-  attr <<<<"EOF"
+def form_binary(version, binaries, value)
+  $attr <<<<"EOF"
   binary_#{version.delete(".")}:
     label: Binary file version #{version}
     widget: select
@@ -560,21 +561,21 @@ def form_binary(attr, version, binaries, value)
     options:
 EOF
   binaries.each do |b|
-    attr << "      - " + b + "\n"
+    $attr << "      - " + b + "\n"
   end
 
   return "- binary_" + version.delete(".")
 end
 
-def form_filename(attr, memo = "", requred = false)
-  attr << "  filename:\n"
+def form_filename(memo = "", requred = false)
+  $attr << "  filename:\n"
   if memo == ""
-    attr << "    label: Input file\n"
+    $attr << "    label: Input file\n"
   else
-    attr << "    label: Input file (#{memo})\n"
+    $attr << "    label: Input file (#{memo})\n"
   end
 
-  attr <<<<"EOF"
+  $attr <<<<"EOF"
     data-filepicker: true
     data-target-file-type: files  # Valid values are: files, dirs, or both
     # Optionally set a default directory
@@ -582,13 +583,13 @@ def form_filename(attr, memo = "", requred = false)
     # Optionally only allow editing through the file picker; defaults to false
     data-file_picker_favorites: #{get_groups_fdirs()}
 EOF
-  attr << "    required: true\n" if requred
+  $attr << "    required: true\n" if requred
 
   return "- filename"
 end
 
-def form_working_dir(attr)
-  attr <<<<"EOF"
+def form_working_dir()
+  $attr <<<<"EOF"
   working_dir:
     label: Working directory
     data-filepicker: true
@@ -601,8 +602,8 @@ EOF
   return "- working_dir"
 end
 
-def form_options(attr, memo = "")
-  attr <<<<"EOF"
+def form_options(memo = "")
+  $attr <<<<"EOF"
   options:
     label: Options (#{memo})
     widget: text_field
@@ -610,8 +611,8 @@ EOF
   return "- options"
 end
 
-def form_commands(attr)
-  attr <<<<"EOF"
+def form_commands()
+  $attr <<<<"EOF"
   commands:
     label: Commands (e.g. mpiexec ./a.out)
     widget: text_area
@@ -619,8 +620,8 @@ EOF
   return "- commands"
 end
 
-def form_session_name(attr)
-  attr <<<<"EOF"
+def form_session_name()
+  $attr <<<<"EOF"
   session_name:
     label: Session name
     required: true
@@ -628,26 +629,32 @@ EOF
   return "- session_name"
 end
 
-def form_email(attr, only_start = true)
-  attr <<<<"EOF"
+def form_email(only_start = true)
+  $attr <<<<"EOF"
   email:
     widget: email_field
 EOF
 
   if only_start
-    attr << "    label: Email (You will receive an email when it starts)\n"
+    $attr << "    label: Email (You will receive an email when it starts)\n"
   else
-    attr << "    label: Email (You will receive an email when it starts and ends)\n"
+    $attr << "    label: Email (You will receive an email when it starts and ends)\n"
   end
-  
+
   return "- email"
 end
 
-def form_desktop(attr)
-  attr <<<<"EOF"
+def form_desktop()
+  $attr <<<<"EOF"
   desktop:
     widget: hidden_field
     value: xfce
 EOF
   return "- desktop"
+end
+
+def form_attr()
+  attr = $attr.dup
+  $attr = ""
+  return attr
 end
