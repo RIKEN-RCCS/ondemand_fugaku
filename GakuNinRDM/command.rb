d970697715a14af116d860158b01db15d03a9095
Masahiro Nakao <masahiro.nakao@riken.jp>
2023-09-05 16:46:28 +0900

    minor fix

--- GakuNinRDM/command.rb
+++ GakuNinRDM/command.rb
@@ -1,47 +1,42 @@
 require 'yaml'
 require 'fileutils'
-SAVE_DIR  = Dir.home + "/ondemand/"
-SAVE_FILE = SAVE_DIR + "gakunin_rdm.yml"
+SAVE_FILE   = Dir.home + "/ondemand/gakunin_rdm.yml"
+RDM_API_URL = "https://api.rdm.nii.ac.jp/v2/"
 
 class Command
-  @err = nil
   def exec(params)
-    save_flag = false
     FileUtils.touch(SAVE_FILE)
-
     data = open(SAVE_FILE, 'r') { |f| YAML.load(f) }
     data = Hash.new if data == false
 
     if params != nil then
       if params['act'] == 'mount' then
-        if `mount | grep #{params['mount_path']} | wc -l`.strip == '0' then
-          `env RDM_API_URL="https://api.rdm.nii.ac.jp/v2/" RDM_NODE_ID=#{params['rdm_node_id']} RDM_TOKEN=#{params['rdm_token']} MOUNT_PATH=#{params['mount_path']} /usr/local/sbin/rdmfs_mount.sh &> /dev/null &`
-          `sleep 1`
+        unless system("mount | grep #{params['mount_path']}") then
+          system("env RDM_API_URL=#{RDM_API_URL} RDM_NODE_ID=#{params['rdm_node_id']} RDM_TOKEN=#{params['rdm_token']} MOUNT_PATH=#{params['mount_path']} /usr/local/sbin/rdmfs_mount.sh &> /dev/null &")
+          system("sleep 1")
           data.delete(params['mount_path'])
           d = Hash.new
           d['rdm_node_id'] = params['rdm_node_id']
           d['rdm_token']   = params['rdm_token']
           data[params['mount_path']] = d
-          save_flag = true
+          open(SAVE_FILE, 'w') {|f| YAML.dump(data, f) }
         end
       elsif params['act'] == 'unmount' then
-        `fusermount3 -u #{params['mount_path']}`
+        system("fusermount3 -u #{params['mount_path']}")
       elsif params['act'] == 'delete' then
         data.delete(params['mount_path'])
-        save_flag = true
+        open(SAVE_FILE, 'w') {|f| YAML.dump(data, f) }
       end
     end
 
-    if save_flag then
-      open(SAVE_FILE, 'w') {|f| YAML.dump(data, f) }
-    end
-
     data.each do |k, v|
-      real_path    = `readlink -f #{k}`.strip
-      mount_action = `mount | grep #{real_path} | wc -l`.strip == '0' ? 'mount' : 'unmount'
-      data[k]['available_action'] = mount_action
+      if Dir.exist?(k) then
+        system("fusermount3 -u #{k}") unless system("ls #{k}") # Check if it is mounted correctly
+      end
+      real_path = `readlink -f #{k}`.strip
+      data[k]['available_action'] = system("mount | grep #{real_path}")? 'unmount' : 'mount'
     end
 
-    [data, @err]
+    [data, nil]
   end
 end
