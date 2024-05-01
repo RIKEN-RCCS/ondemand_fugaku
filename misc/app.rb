082f7f491d592e9a0bcce2f6a1b60a0aeb02f691
Masahiro Nakao <masahiro.nakao@riken.jp>
2024-05-01 14:01:09 +0900

    Fix MODYLAS for spack v0.21

--- misc/app.rb
+++ misc/app.rb
@@ -11,7 +11,7 @@ app.push(App.new("quantum_espresso71", "/vol0004/apps/opt/qe-7.1/bin"))
 app.push(App.new("ab2-4", "/vol0004/apps/opt/SPACK-Feb2023-ABINIT-MP-VER2-REV4/bin"))
 app.push(App.new("ab1-22", "/vol0004/apps/opt/SPACK-Feb2023-ABINIT-MP-VER1-REV22/bin"))
 app.push(App.new("openmx39", "/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/openmx-3.9-j52bvdtvorjy2stvpf7ve2ygxogwjw4n/bin/"))
-app.push(App.new("modylas110b", "/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/modylas-new-1.1.0b-sjr6ygmvjq32hkctieja7j6wgl3d7l7p/bin/"))
+app.push(App.new("modylas110", "/vol0004/apps/oss/spack-v0.21/opt/spack/linux-rhel8-a64fx/fj-4.10.0/modylas-new-1.1.0-qpbwc72nrq36nszxjpwhhbwjcmgbob33/bin/"))
 app.push(App.new("fugaku-frontistr_master", "/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/fugaku-frontistr-master-rqgydh3e5t6ckad7qkjmujzuepde7qza/bin"))
 #############
 
