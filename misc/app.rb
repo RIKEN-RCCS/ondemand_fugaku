d0302ade9e062e5f3b72dad6999aab5ff63efe35
Masahiro Nakao <masahiro.nakao@riken.jp>
2024-05-01 12:05:21 +0900

    Fix for spack v0.21

--- misc/app.rb
+++ misc/app.rb
@@ -5,12 +5,9 @@ CACHE_DIR = "/system/ood/app/"
 App = Struct.new(:file, :path)
 
 app = []
+app.push(App.new("quantum_espresso73", "/vol0004/apps/opt/qe-7.3/bin"))
 app.push(App.new("quantum_espresso72", "/vol0004/apps/opt/qe-7.2/bin"))
 app.push(App.new("quantum_espresso71", "/vol0004/apps/opt/qe-7.1/bin"))
-app.push(App.new("quantum_espresso68", "/vol0004/apps/opt/qe-6.8/bin"))
-app.push(App.new("quantum_espresso67", "/vol0004/apps/opt/qe-6.7/bin"))
-#app.push(App.new("quantum_espresso66", "/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/quantum-espresso-6.6-d2bgjxrq6wj4so7wehupzvqkg5u7nk7y/bin/"))
-#app.push(App.new("quantum_espresso65", "/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/quantum-espresso-6.5-as6kxmc5wvgscu2xqlsfb6akkbjzf3fz/bin/"))
 app.push(App.new("ab2-4", "/vol0004/apps/opt/SPACK-Feb2023-ABINIT-MP-VER2-REV4/bin"))
 app.push(App.new("ab1-22", "/vol0004/apps/opt/SPACK-Feb2023-ABINIT-MP-VER1-REV22/bin"))
 app.push(App.new("openmx39", "/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/openmx-3.9-j52bvdtvorjy2stvpf7ve2ygxogwjw4n/bin/"))
