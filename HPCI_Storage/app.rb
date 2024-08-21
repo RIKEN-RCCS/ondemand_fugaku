438d0a35f378633263b71755b26873a1a3653fad
Masahiro Nakao <masahiro.nakao@riken.jp>
2024-08-21 16:39:04 +0900

    Switch OAuth from GSI for HPCI_Storage

--- HPCI_Storage/app.rb
+++ HPCI_Storage/app.rb
@@ -1,83 +1,58 @@
+# coding: utf-8
 require 'yaml'
 require 'fileutils'
-
 set :erb, :escape_html => true
 
 GFARM_CONF = Dir.home + "/.gfarm2rc"
 SAVE_FILE  = Dir.home + "/ondemand/hpci.yml"
-HPCI_URL   = "portal.hpci.nii.ac.jp"
+JWT_AGENT="jwt-agent -s https://elpis.hpci.nii.ac.jp/ -l"
 
-helpers do
-  def dashboard_title
-    "Open OnDemand"
-  end
-
-  def dashboard_url
-    "/pun/sys/dashboard/"
-  end
-
-  def title
-    "HPCI Shared Storage"
-  end
-end
-
-def gfarm_exec(params)
+post '/' do
   # Encrypted communication settings
-  unless File.exist?(GFARM_CONF) then
-    File.open(GFARM_CONF, 'w') {|f|
-      f.write("auth enable gsi *\n")
-      f.write("auth disable gsi_auth *\n")
-    }
+  unless File.exist?(GFARM_CONF)
+    File.open(GFARM_CONF, 'w') { |f| f.write("auth enable sasl_auth *\n") }
   end
 
-  FileUtils.touch(SAVE_FILE)
-  FileUtils.chmod(0600, SAVE_FILE)
-  data = open(SAVE_FILE, 'r') { |f| YAML.load(f) }
-  data = Hash.new if data.nil?
-
-  if params != nil then
-    if params['act'] == 'mount' then
-      system("echo #{params['pass']} | myproxy-logon -s #{HPCI_URL} -l #{params['hpci_id']} -t #{params['time']} -S")
-      mount_path = []
-      IO.popen("mount.hpci | grep 'Mount GfarmFS on' | grep #{params['hpci_id']} | awk '{print $4}'") do |io|
-        io.each_line do |line|
-          mount_path << line.chomp
-        end
-      end
-
-      d = Hash.new
-      d['mount_path'] = mount_path
-      d['hpci_id']    = params['hpci_id']
-      d['time']       = params['time']
-      data[params['hpci_id']] = d
-      open(SAVE_FILE, 'w') {|f| YAML.dump(data, f) }
-    elsif params['act'] == 'unmount' then
-      system("echo #{params['pass']} | myproxy-logon -s #{HPCI_URL} -l #{params['hpci_id']} -t #{params['time']} -S")
-      system("umount.hpci")
-    elsif params['act'] == 'delete' then
-      data.delete(params['hpci_id'])
-      open(SAVE_FILE, 'w') {|f| YAML.dump(data, f) }
-    end
+  @data = Hash.new
+  case params['act']
+  when 'mount'
+    p params
+    `echo #{params['pass']} | #{JWT_AGENT} #{params['user']}`
+    output = `mount.hpci`
+    # The output is "Mount GfarmFS on /tmp/hp120273/hpci003062" or
+    # "/tmp/hp120273/hpci003062: already mounted".
+    # Extract only the mount path.
+    @data['path'] = output.match(/\/tmp\/\S+/)[0].gsub(/:$/, '') if output =~ /\/tmp\/\S+/
+    @data['user'] = params['user']
+    p @data
+    open(SAVE_FILE, 'w') { |f| YAML.dump(@data, f) }
+    @data['next'] = "umount"
+  when 'umount'
+    `umount.hpci`
+    @data = (File.exist?(SAVE_FILE))? open(SAVE_FILE, 'r') { |f| YAML.load(f) } : nil
+    @data = nil if @data.empty?
+    @data['next'] = "mount" unless @data.nil?
+  when 'delete'
+    @data = Hash.new
+    open(SAVE_FILE, 'w') { |f| YAML.dump(@data, f) }
+    `umount.hpci`
+    @data['next'] = "mount"
   end
-
-  data.each do |k, v|
-    data[k]['act'] = system("mount | grep #{data[k]['mount_path'][0]}")? 'unmount' : 'mount'
-  end
-
-  return data
-end
-
-# Define a route at the root '/' of the app.
-get '/' do
-  @data = gfarm_exec(nil)
-
-  # Render the view
+  
   erb :index
 end
 
-post '/' do
-  @data = gfarm_exec(params)
+get '/' do
+  if File.exist?(SAVE_FILE)
+    @data = open(SAVE_FILE, 'r') { |f| YAML.load(f) }
+    unless @data.empty?
+      @data['next'] = system("mount | grep #{@data['path']}")? 'umount' : 'mount'
+    else
+      @data = nil
+    end
+  else
+    @data = nil
+  end
 
-  # Render the view
   erb :index
 end
--- HPCI_Storage/config.ru
+++ HPCI_Storage/config.ru
@@ -1,5 +1,3 @@
 require 'sinatra'
-
 require './app'
-
 run Sinatra::Application
